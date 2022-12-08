6116ef27206be90a3a4b3ddcaf5a8ec9799b4ffc
mamico <mauro.amico@gmail.com>
2022-12-08 16:39:26 +0100

    day..day3

--- day-3/src/main.zig
+++ day-3/src/main.zig
@@ -1,4 +1,4 @@
-// part1
+// part1 + part2
 
 const std = @import("std");
 
@@ -20,29 +20,70 @@ pub fn priority_1(rucksack: []u8) u32 {
     return 0;
 }
 
+pub fn priority_2(rs1: []u8, rs2: []u8, rs3: []u8) u32 {
+    for (rs1) |c1| {
+        for (rs2) |c2| {
+            if (c1 == c2) {
+                for (rs3) |c3| {
+                    if (c3 == c1) {
+                        if (c1 >= 'a' and c1 <= 'z') {
+                            return c1 - 'a' + 1;
+                        }
+                        else {  // A-Z
+                            return c1 - 'A' + 27;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
+
 pub fn main() !void {
     const stdin = std.io.getStdIn();
     defer stdin.close();
     var buffer: [64]u8 = undefined;
     var reader = stdin.reader();
+    var idx: u8 = 0;
     var t1: u32 = 0;
+    var t2: u32 = 0;
+    var r = [3][64]u8{ undefined, undefined, undefined };
     while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
         t1 += priority_1(line);
+        std.mem.copy(u8, &r[idx], line);
+        if (idx == 2) {
+            t2 += priority_2(&r[0], &r[1], &r[2]);
+            r = [3][64]u8{undefined, undefined, undefined};
+        }
+        idx = (idx + 1) % 3;
     }
     std.debug.print("Sum of priorities part 1 {}.\n", .{t1});
+    std.debug.print("Sum of priorities part 2 {}.\n", .{t2});
 }
 
 test "test part 1" {
-    var pack1 = "vJrwpWtwJgWrhcsFMMfFFhFp".*;
-    try std.testing.expectEqual(@as(u32, 16), priority_1(&pack1));
-    var pack2 = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".*;
-    try std.testing.expectEqual(@as(u32, 38), priority_1(&pack2));
-    var pack3 = "PmmdzqPrVvPwwTWBwg".*;
-    try std.testing.expectEqual(@as(u32, 42), priority_1(&pack3));
-    var pack4 = "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn".*;
-    try std.testing.expectEqual(@as(u32, 22), priority_1(&pack4));
-    var pack5 = "ttgJtRGJQctTZtZT".*;
-    try std.testing.expectEqual(@as(u32, 20), priority_1(&pack5));
-    var pack6 = "CrZsJsPPZsGzwwsLwLmpwMDw".*;
-    try std.testing.expectEqual(@as(u32, 19), priority_1(&pack6));
+    var rucksack1 = "vJrwpWtwJgWrhcsFMMfFFhFp".*;
+    var rucksack2 = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".*;
+    var rucksack3 = "PmmdzqPrVvPwwTWBwg".*;
+    var rucksack4 = "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn".*;
+    var rucksack5 = "ttgJtRGJQctTZtZT".*;
+    var rucksack6 = "CrZsJsPPZsGzwwsLwLmpwMDw".*;
+    try std.testing.expectEqual(@as(u32, 16), priority_1(&rucksack1));
+    try std.testing.expectEqual(@as(u32, 38), priority_1(&rucksack2));
+    try std.testing.expectEqual(@as(u32, 42), priority_1(&rucksack3));
+    try std.testing.expectEqual(@as(u32, 22), priority_1(&rucksack4));
+    try std.testing.expectEqual(@as(u32, 20), priority_1(&rucksack5));
+    try std.testing.expectEqual(@as(u32, 19), priority_1(&rucksack6));
+}
+
+test "test part 2" {
+    var rucksack1 = "vJrwpWtwJgWrhcsFMMfFFhFp".*;
+    var rucksack2 = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".*;
+    var rucksack3 = "PmmdzqPrVvPwwTWBwg".*;
+    var rucksack4 = "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn".*;
+    var rucksack5 = "ttgJtRGJQctTZtZT".*;
+    var rucksack6 = "CrZsJsPPZsGzwwsLwLmpwMDw".*;
+    try std.testing.expectEqual(@as(u32, 18), priority_2(&rucksack1, &rucksack2, &rucksack3));
+    try std.testing.expectEqual(@as(u32, 52), priority_2(&rucksack4, &rucksack5, &rucksack6));
 }
