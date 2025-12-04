20c827e23eb9c26ddb5a6deb61ed60aafccea877
Mauro Amico <mauro.amico@gmail.com>
2025-12-04 23:30:19 +0100

    d4

--- 2025/day-04/main.v
+++ 2025/day-04/main.v
@@ -44,47 +44,47 @@ fn main() {
 
 	// PART 2
 	{
-	mut grid := [][]int{}
-	mut total := 0
-	for line in os.read_lines(os.args[1])! {
-		grid << line.split("").map(
-			fn (it string) int {
-				if it == '.' { return 9 } else { return 0 } 
-			}
-		)
-	}
-	for true {
-		for i in 0..grid[0].len {
-			for j in 0..grid.len {
-				for ii in [i-1, i, i+1] {
-					for jj in [j-1, j, j+1] {
-						if ii == i && jj == j {
-							continue
-						}
-						if ii >= 0 && ii < grid[0].len && jj >= 0 && jj < grid.len && grid[ii][jj] < 9 {
-							grid[i][j]++
+		mut grid := [][]int{}
+		mut total := 0
+		for line in os.read_lines(os.args[1])! {
+			grid << line.split("").map(
+				fn (it string) int {
+					if it == '.' { return 9 } else { return 0 } 
+				}
+			)
+		}
+		for true {
+			for i in 0..grid[0].len {
+				for j in 0..grid.len {
+					for ii in [i-1, i, i+1] {
+						for jj in [j-1, j, j+1] {
+							if ii == i && jj == j {
+								continue
+							}
+							if ii >= 0 && ii < grid[0].len && jj >= 0 && jj < grid.len && grid[ii][jj] < 9 {
+								grid[i][j]++
+							}
 						}
 					}
 				}
 			}
-		}
-		mut f := 0
-		for i in 0..grid[0].len {
-			for j in 0..grid.len {
-				if grid[i][j] < 4 {
-					f++
-					grid[i][j] = 9
-				}
-				else if grid[i][j] < 9{
-					grid[i][j] = 0
+			mut f := 0
+			for i in 0..grid[0].len {
+				for j in 0..grid.len {
+					if grid[i][j] < 4 {
+						f++
+						grid[i][j] = 9
+					}
+					else if grid[i][j] < 9{
+						grid[i][j] = 0
+					}
 				}
 			}
+			if f == 0 {
+				break
+			}
+			total += f
 		}
-		if f == 0 {
-			break
-		}
-		total += f
+		println('Part 2 - ${total}')
 	}
-	println('Part 2 - ${total}')
-}
 }
